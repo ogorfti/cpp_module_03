f316cf9ea3e11bc9278d34d20db886d93a7ff5b7
ogorfti <ww.shanks1@gmail.com>
2023-10-09 13:34:59 +0100

    ex00 done

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ogorfti <ogorfti@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 11:44:01 by ogorfti           #+#    #+#             */
+/*   Updated: 2023/10/09 13:23:36 by ogorfti          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+/*-------------------- Orthodox --------------------*/
+
+ClapTrap::ClapTrap()
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string newName)
+{
+	this->name = newName;
+	this->hitPoints = 10;
+	this->energyPoints = 10;
+	this->attackDamage = 10;
+}
+
+ClapTrap& ClapTrap::operator=(ClapTrap& other)
+{
+	if (&other != this)
+	{
+		this->name = other.name;
+		this->hitPoints = other.hitPoints;
+		this->energyPoints = other.energyPoints;
+		this->attackDamage = other.attackDamage;
+		std::cout << "Copy assignment operator called" << std::endl;
+	}
+	return (*this);
+}
+
+ClapTrap::ClapTrap(ClapTrap& other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+/*-------------------- Member functions --------------------*/
+
+void ClapTrap::attack(const std::string& target)
+{
+	std::cout << "ClapTrap " << this->name << " attacks " << target << ", causing " << this->attackDamage << " points of damage" << std::endl;
+	this->energyPoints -= 1;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (this->hitPoints > 0)
+	{
+		std::cout << "ClapTrap " << this->name << " has taken " << amount << " hit points of damage" << std::endl;
+		this->hitPoints -= amount;
+	}
+	else
+		std::cout << "ClapTrap " << this->name << " is Death" << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->energyPoints > 0)
+	{
+		std::cout << "ClapTrap" << this->name << " has repaired itself, gaining  " << amount << " hit points" << std::endl;
+		this->energyPoints -= 1;
+		this->hitPoints += amount;
+	}
+}
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,8 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ogorfti <ogorfti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/09 10:21:06 by ogorfti           #+#    #+#             */
-/*   Updated: 2023/10/09 10:21:07 by ogorfti          ###   ########.fr       */
+/*   Created: 2023/10/09 12:06:52 by ogorfti           #+#    #+#             */
+/*   Updated: 2023/10/09 13:32:44 by ogorfti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+int main()
+{
+	ClapTrap clap("Gon");
+	ClapTrap hxh;
+
+	hxh = clap;
+	hxh.attack("Neferpitou");
+	hxh.attack("Neferpitou");
+	hxh.beRepaired(50);
+	hxh.takeDamage(20);
+	hxh.takeDamage(20);
+	hxh.takeDamage(20);
+	hxh.takeDamage(20);
+}
